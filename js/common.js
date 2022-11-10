d9be6f694c86bf17943c96a6205f439388b4bde5
bzirs <bzirs@outlook.com>
2022-11-10 17:01:22 +0800

    注册功能实现

--- js/common.js
+++ js/common.js
@@ -1,17 +1,24 @@
 // 上面这个代码处理过度动画（默认加上不用管）
-document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    document.body.classList.add('sidenav-pinned')
-    document.body.classList.add('ready')
-  }, 200)
-})
+document.addEventListener("DOMContentLoaded", () => {
+	setTimeout(() => {
+		document.body.classList.add("sidenav-pinned");
+		document.body.classList.add("ready");
+	}, 200);
+});
 
 // 创建bootstrap弹窗
-let toastBox = document.querySelector('#myToast')
+let toastBox = document.querySelector("#myToast");
 const toast = new bootstrap.Toast(toastBox, {
-  animate: true,
-  autohide: true,
-  delay: 3000
-})
+	animate: true,
+	autohide: true,
+	delay: 3000,
+});
 
+// 封装显示提示信息
+let setHint = (val) => {
+	toastBody.innerHTML = val;
+	toast.show();
+};
 
+// 配置axios基地址
+axios.defaults.baseURL = "http://ajax-api.itheima.net";
